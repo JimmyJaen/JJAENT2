63790d1fb97faee437562c61cd5d8bb5bd9b8954
jaenb <jaenb@JIMMY>
2024-05-05 23:11:58 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JJAENT2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Vistas.Inicio();
+        }
+    }
+}
